ed7898e274784f409c11a21e281f5c2cb3211c4f
Paul Chalmers <pchalmers2011@gmail.com>
2019-11-05 15:25:55 -0500

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int doubleNum(int a, int b)
+        {
+            return a * b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The answer is - {0}", doubleNum(2, 4));
+        }
+    }
+}
---
+++ WebApplication1/webAppTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace webAppTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
